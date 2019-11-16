d03e3749211ce2012f72e1b493b2399f34bc76c7
NguyenNgocThai <thaind1994@gmail.com>
2019-11-16 10:49:59 +0700

    Fix Dropmenu

--- component/nav.js
+++ component/nav.js
@@ -5,4 +5,8 @@ $(document).ready(function(){
     $('.menu-toggle').click(function(){
         $('nav').toggleClass('active')
     })
+    $('ul li').click(function(){
+        $(this).siblings().removeClass('active');
+        $(this).toggleClass('active');
+    })
 })
